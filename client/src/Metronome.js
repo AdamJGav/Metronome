5931e99970fb880993616db422a06cbe1b35cbe8
Adam Gavigan <ajgavigan@hotmail.co.uk>
2024-11-21 16:08:20 +0000

    add tick logic

--- client/src/Metronome.js
+++ client/src/Metronome.js
@@ -7,22 +7,17 @@ const Metronome = () => {
   const [bpm, setBpm] = useState(100); // Default BPM
   const [isRunning, setIsRunning] = useState(false); // Track if the metronome is running
   const intervalRef = useRef(null); // Reference to store the interval ID
+  let beatNumber = useRef(0);
 
   // Calculate the interval time in milliseconds
-  const intervalTime = 60000 / bpm;
+  let intervalTime = 60000 / bpm;
 
   // Start the metronome
   const startMetronome = () => {
     if (isRunning) return; // Prevent starting multiple times
 
     setIsRunning(true);
-
-    intervalRef.current = setInterval(() => {
-      console.log('tick')
-      // beepSound.play().catch((error) => {
-      //   console.log("Audio play failed:", error);
-      // });
-    }, intervalTime);
+    startTickLoop()
   };
 
   // Stop the metronome
@@ -42,19 +37,29 @@ const Metronome = () => {
   const handleBpmChange = (event) => {
     const newBpm = event.target.value;
     setBpm(newBpm);
+    intervalTime = 60000 / newBpm;
+    beatNumber = 1;
 
     // If the metronome is running, reset the interval with the new BPM
     if (isRunning) {
       clearInterval(intervalRef.current);
-      intervalRef.current = setInterval(() => {
-        console.log('tick')
-        // beepSound.play();
-      }, 60000 / newBpm);
+      startTickLoop()
     }
   };
 
+  const startTickLoop = () => {
+    intervalRef.current = setInterval(() => {
+      beatNumber = beatNumber < 4 ? beatNumber+1 : 1;
+      let sound = beatNumber == 1 ? 'TICK' : 'tick';
+      console.log(sound)
+      // beepSound.play().catch((error) => {
+      //   console.log("Audio play failed:", error);
+      // });
+    }, intervalTime);
+  }
+
   return (
-    <div class="metronome">
+    <div className="metronome">
       <h1>MyMetronome</h1>
 
       {/* BPM Display and Slider */}
